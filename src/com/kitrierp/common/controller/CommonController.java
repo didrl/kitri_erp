ddc6e7495b07ec3e6926d656b5ef80e12046121f
didrl3382@gmail.com <didrl3382@gmail.com>
2016-07-14 14:54:36 +0900

    ex) 문서번호 생성

--- src/com/kitrierp/common/controller/CommonController.java
+++ src/com/kitrierp/common/controller/CommonController.java
@@ -2,11 +2,23 @@ package com.kitrierp.common.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kitrierp.common.model.service.CommonService;
 
 @Controller
+@RequestMapping("/common")
 public class CommonController {
 	@Autowired
 	private CommonService commonService;
+	
+	@RequestMapping("/doc_id.erp")
+	public String docId() {
+		String doc_id = "";
+		int doc_type_id = 01;
+		doc_id = commonService.doc_id(doc_type_id);
+		System.out.println(doc_id);
+		return doc_id;
+	}
+	
 }
