6554215ef56107808e7dc8695bda374c04c71382
didrl3382@gmail.com <didrl3382@gmail.com>
2016-07-14 14:55:12 +0900

    결재 준비중

--- src/com/kitrierp/doc/controller/DocControlloer.java
+++ src/com/kitrierp/doc/controller/DocControlloer.java
@@ -2,11 +2,19 @@ package com.kitrierp.doc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.kitrierp.doc.model.service.DocService;
 
+@SessionAttributes("memberInfo")
+@RequestMapping("/doc")
 @Controller 
 public class DocControlloer {
 	@Autowired
 	private DocService docService;
+	
+//	@RequestMapping("/")
+	
+	
 }
