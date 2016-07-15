a37be659b8d476b675412802fc447e056493161c
zms0529 <zms0529@google.com>
2016-07-15 09:36:30 +0900

    [FIX] session 사용 수정중

--- src/com/kitrierp/doc/controller/DocControlloer.java
+++ src/com/kitrierp/doc/controller/DocControlloer.java
@@ -1,5 +1,6 @@
 package com.kitrierp.doc.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.util.WebUtils;
 
 import com.kitrierp.doc.model.service.DocService;
 
@@ -18,33 +20,33 @@ public class DocControlloer {
 	private DocService docService;
 	
 	@RequestMapping(value="/mvBtripApplication.erp", method=RequestMethod.GET)
-	public String mvBtripApplication(HttpSession session){
-		session.setAttribute("docType", 4);
+	public String mvBtripApplication(HttpServletRequest request){
+		WebUtils.setSessionAttribute(request, "docType", 4);
 		return "redirect:/doc/document/btrip_application.jsp";
 	}
 	@RequestMapping(value="/mvBtripReport.erp", method=RequestMethod.GET)
-	public String mvBtripReport(HttpSession session){
-//		session.setAttribute("docType", 5);
-		return "redirect:/doc/document/btrip_report.jsp?docType=5";
+	public String mvBtripReport(HttpServletRequest request){
+		WebUtils.setSessionAttribute(request, "docType", 5);
+		return "redirect:/doc/document/btrip_report.jsp";
 	}
 	@RequestMapping(value="/mvConsultation.erp", method=RequestMethod.GET)
-	public String mvConsultation(HttpSession session){
-		session.setAttribute("docType", 2);
+	public String mvConsultation(HttpServletRequest request){
+		WebUtils.setSessionAttribute(request, "docType", 2);
 		return "redirect:/doc/document/consultation.jsp";
 	}
 	@RequestMapping(value="/mvPayment.erp", method=RequestMethod.GET)
-	public String mvPayment(HttpSession session){
-		session.setAttribute("docType", 3);
+	public String mvPayment(HttpServletRequest request){
+		WebUtils.setSessionAttribute(request, "docType", 3);
 		return "redirect:/doc/document/payment.jsp";
 	}
 	@RequestMapping(value="/mvProposal.erp", method=RequestMethod.GET)
-	public String mvProposal(HttpSession session){
-		session.setAttribute("docType", 1);
+	public String mvProposal(HttpServletRequest request){
+		WebUtils.setSessionAttribute(request, "docType", 1);
 		return "redirect:/doc/document/proposal.jsp";
 	}
 	@RequestMapping(value="/mvJounal.erp", method=RequestMethod.GET)
-	public String mvJounal(HttpSession session){
-		session.setAttribute("docType", 7);
+	public String mvJounal(HttpServletRequest request){
+		WebUtils.setSessionAttribute(request, "docType", 7);
 		return "redirect:/doc/document/jounal.jsp";
 	}
 	
