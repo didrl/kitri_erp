5b7e89ccf261ba632395af78516923677f5a7238
zms0529 <zms0529@google.com>
2016-07-22 13:38:28 +0900

    [View] header, 문서 jsp 수정 (DB와 변수 이름 맞춤) doc.js 파일 alert 삭제

--- WebContent/js/doc-js.js
+++ WebContent/js/doc-js.js
@@ -2,18 +2,15 @@ $().ready(function() {
 	
 	$('#reportDoc').click(function(){
 		$('#doc_status_id').val(1);
-		alert("상신 되었습니다.  "+$('#doc_status_id').val);
 		$('#docform').attr({action: root+controller+"/reportDoc.erp", method:'post'}).submit();
 	});
 	
 	$('#tmpsaveDoc').click(function(){
-		alert("임시저장 되었습니다.");
 		$('#doc_status_id').val(0);
 		document.docform.action = root+controller+"/tmpsaveDoc.erp";
 		document.docform.submit();
 	});
 	$('#modifyDoc').click(function(){
-		alert("수정 :    "+root+controller+"/modifyDoc.erp?doc_id="+doc_id);
 		document.location.href = root+controller+"/modifyDoc.erp?doc_id="+doc_id;
 	});
 	
